067ceab9b6915081b90ba43bf89a0cbc3ed0430e
Lekan Nicholson <lekan.nicholson@gmail.com>
2017-07-11 01:01:54 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
+function nestedTarget(){
+  return document.querySelector('div')
+}
+function deepestChild(){
+  return document.querySelector('#grand-node div div div div')
+}
+function increaseRankBy(n){
+  var list = document.querySelectorAll('ul.ranked-list li')
+
+  for (var i = 0; i < list.length; i++){
+    list[i].innerHTML = parseInt(list[i].innerHTML) + n
+  }
+  return list
+}
